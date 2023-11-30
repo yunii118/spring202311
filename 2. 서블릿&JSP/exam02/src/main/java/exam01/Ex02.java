206f9ac38fe6c86f45bbda67b8d70c0d1c18ec1d
yunii118 <lyhee118@gmail.com>
2023-11-30 11:23:19 +0900

    setter getter

---
+++ 2. 서블릿&JSP/exam02/src/main/java/exam01/Ex02.java
@@ -0,0 +1,13 @@
+package exam01;
+
+public class Ex02 {
+
+    public static void main(String[] args) {
+        Book b1 = new Book();
+        b1.setTitle("스프링5");
+        b1.setAuthor("최범균");
+        b1.setPublisher("가메출판사");
+
+        b1.toString();
+    }
+}
--- 2. 서블릿&JSP/exam02/src/main/java/exam01/Schedule.java
+++ 2. 서블릿&JSP/exam02/src/main/java/exam01/Schedule.java
@@ -1,11 +1,45 @@
-pagkage exam01;
+package exam01;
 
-public class Schedule{
-    int year;
-    int month;
-    int day;
+public class Schedule {
+    private int year;
+    private int month;
+    private int day;
+
+    public int getYear() {
+        return year;
+    }
+
+    public void setYear(int year) {
+        this.year = year;
+    }
+
+    public int getMonth() {
+        return month;
+    }
+
+    public void setMonth(int month) {
+        this.month = month;
+    }
+
+    public int getDay() {
+        return day;
+    }
+
+    public void setDay(int day) {
+        if(day > 28) day = 28;
+        this.day = day;
+    }
 
     void showDate(){
         System.out.printf("year = %d, month = %d, day = %d%n", year, month, day);
+    }
+
+    @Override
+    public String toString() {
+        return "Schedule{" +
+                "year=" + year +
+                ", month=" + month +
+                ", day=" + day +
+                '}';
     }
 }
