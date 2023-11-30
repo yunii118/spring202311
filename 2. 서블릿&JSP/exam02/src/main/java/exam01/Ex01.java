7446ec7994b3f6ba10474e000098a5e8cb533ef3
yunii118 <lyhee118@gmail.com>
2023-11-30 10:45:49 +0900

    java exam 추가

---
+++ 2. 서블릿&JSP/exam02/src/main/java/exam01/Ex01.java
@@ -0,0 +1,13 @@
+package exam01;
+
+public class Ex01{
+
+    public static void main(String[] args) {
+        Schedule s1  = new Schedule();
+        s1.year = 2023;
+        s1.month = 11;
+        s1.day = 30;
+
+        s1.showDate();
+    }
+}
